9a558e67f939a557cd2879aa51cc772336d2337e
박서연 <162225988+o25e@users.noreply.github.com>
2025-09-03 16:42:36 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if 89 < score <= 100:
+    print("A")
+elif 79 < score < 90:
+    print("B")
+elif 69 < score < 80:
+    print("C")
+elif 59 < score < 70:
+    print("D")
+else:
+    print("F")
