ca6f095cbf1f777eab0092f8a59a87d6903b9e06
박서연 <162225988+o25e@users.noreply.github.com>
2026-03-15 21:19:37 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -2,23 +2,26 @@ x1, y1 = map(int, input().split())
 x2, y2 = map(int, input().split())
 x3, y3 = map(int, input().split())
 
-if (abs(x1 -x2) == 0) and (abs(y3 - y1) == 0):
-    x4 = x3
-    y4 = y2
-elif (abs(x1 - x2) == 0) and (abs(y3 - y2) == 0):
-    x4 = x3
-    y4 = y1
-elif (abs(x1 - x3) == 0) and (abs(y2 - y1) == 0):
-    x4 = x2
-    y4 = y3
-elif (abs(x1 - x3) == 0) and (abs(y2 - y3) == 0):
-    x4 = x2
-    y4 = y1
-elif (abs(x2 - x3) == 0) and (abs(y1 - y2) == 0):
-    x4 = x1
-    y4 = y3
-elif (abs(x2 - x3) == 0) and (abs(y1 - y3) == 0):
-    x4 = x1
-    y4 = y2
-    
+# if (abs(x1 -x2) == 0) and (abs(y3 - y1) == 0):
+#     x4 = x3
+#     y4 = y2
+# elif (abs(x1 - x2) == 0) and (abs(y3 - y2) == 0):
+#     x4 = x3
+#     y4 = y1
+# elif (abs(x1 - x3) == 0) and (abs(y2 - y1) == 0):
+#     x4 = x2
+#     y4 = y3
+# elif (abs(x1 - x3) == 0) and (abs(y2 - y3) == 0):
+#     x4 = x2
+#     y4 = y1
+# elif (abs(x2 - x3) == 0) and (abs(y1 - y2) == 0):
+#     x4 = x1
+#     y4 = y3
+# elif (abs(x2 - x3) == 0) and (abs(y1 - y3) == 0):
+#     x4 = x1
+#     y4 = y2
+
+x4 = x1 ^ x2 ^ x3
+y4 = y1 ^ y2 ^ y3
+
 print(x4, y4)
